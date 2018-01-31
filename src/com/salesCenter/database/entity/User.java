e7fb81f154a62c7f28557729fdbec89256f3c313
afaherty27 <afaherty27@gmail.com>
2018-01-31 01:19:53 -0600

    created inst. vars, zero arg const., overloaded const.

--- src/com/salesCenter/database/entity/User.java
+++ src/com/salesCenter/database/entity/User.java
@@ -6,4 +6,42 @@ package com.salesCenter.database.entity;
  * @version 1.0 on 01/31/2018
  */
 public class User {
+
+    //inst vars relating to DB columns with get & set methods
+    private int      userId;
+    private String   userName;
+    private String   userFirstName;
+    private String   userLastName;
+    private String   userPass;
+    private int      userPhone;
+    private UserRole userRole;
+
+    /**
+     * zero arg constructor
+     * */
+    public User() {
+
+    }
+
+    /**
+     * User object overloaded constructor
+     * @param userId        ref to users id number
+     * @param userName      ref to users username
+     * @param userFirstName ref to users first name
+     * @param userLastName  ref to users last name
+     * @param userPass      ref to users password
+     * @param userPhone     ref to users phone number
+     * @param userRole      ref to users role
+     */
+    public User(int userId, String userName, String userFirstName, String userLastName, String userPass,
+                int userPhone, UserRole userRole) {
+
+        this.userId        = userId;
+        this.userName      = userName;
+        this.userFirstName = userFirstName;
+        this.userLastName  = userLastName;
+        this.userPass      = userPass;
+        this.userPhone     = userPhone;
+        this.userRole      = userRole;
+    }
 }
