f2f16717b2422bdb3c5df5575da459369063fab3
afaherty27 <afaherty27@gmail.com>
2018-01-31 00:49:00 -0600

    initial  commit

---
+++ src/com/salesCenter/database/entity/User.java
@@ -0,0 +1,9 @@
+package com.salesCenter.database.entity;
+
+/**
+ * User object JavaBean class
+ * @author afaherty27
+ * @version 1.0 on 01/31/2018
+ */
+public class User {
+}
---
+++ src/com/salesCenter/database/entity/UserRole.java
@@ -0,0 +1,9 @@
+package com.salesCenter.database.entity;
+
+/**
+ * UserRole object JavaBean class
+ * @author afaherty
+ * @version 1.0 on 01/31/2018
+ */
+public class UserRole {
+}
