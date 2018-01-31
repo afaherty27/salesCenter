0fa651b53b0c6b416a3eba9f485bad5d017425b3
afaherty27 <afaherty27@gmail.com>
2018-01-30 22:46:00 -0600

    intial commit

---
+++ src/UserHomeRedirect.java
@@ -0,0 +1,18 @@
+package com.salesCenter;
+
+
+
+/**
+ *
+ * redirect class for user.jsp page
+ * @author Adam Faherty
+ * Date:01/30/2018
+ */
+public class UserHomeRedirect {
+
+    //ref log4j
+
+    //redirect
+
+
+}
