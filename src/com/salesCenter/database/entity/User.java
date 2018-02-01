19d075b375f4513044c86fe2ee9b597f1b0578d4
afaherty27 <afaherty27@gmail.com>
2018-01-31 23:31:00 -0600

    completed getters and setters in User javabean

--- src/com/salesCenter/database/entity/User.java
+++ src/com/salesCenter/database/entity/User.java
@@ -44,4 +44,116 @@ public class User {
         this.userPhone     = userPhone;
         this.userRole      = userRole;
     }
+
+    /**
+     * Retrieve userId value
+     * @return userId
+     */
+    public int getUserId() {
+        return userId;
+    }
+
+    /**
+     * Set value to userId
+     * @param userId    value of user id
+     */
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * Retrieve userName
+     * @return userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * Set value to userName
+     * @param userName  value of userName
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * Retrieve users first name
+     * @return userFirstName
+     */
+    public String getUserFirstName() {
+        return userFirstName;
+    }
+
+    /**
+     * Set value to userFirstName
+     * @param userFirstName value of users first name
+     */
+    public void setUserFirstName(String userFirstName) {
+        this.userFirstName = userFirstName;
+    }
+
+    /**
+     * Retrieve users last name
+     * @return userLastName
+     */
+    public String getUserLastName() {
+        return userLastName;
+    }
+
+    /**
+     * Set value to userLastName
+     * @param userLastName value of users last name
+     */
+    public void setUserLastName(String userLastName) {
+        this.userLastName = userLastName;
+    }
+
+    /**
+     * Retrieve userPass
+     * @return userPass
+     */
+    public String getUserPass() {
+        return userPass;
+    }
+
+    /**
+     * Set value to userPass
+     * @param userPass value of users password
+     */
+    public void setUserPass(String userPass) {
+        this.userPass = userPass;
+    }
+
+    /**
+     * Retrieve userPhone
+     * @return userPhone
+     */
+    public int getUserPhone() {
+        return userPhone;
+    }
+
+    /**
+     * Set value to userPhone
+     * @param userPhone value of users phone
+     */
+    public void setUserPhone(int userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    /**
+     * Retrieve value of userRole
+     * @return userRole
+     */
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    /**
+     * Set value to userRole
+     * @param userRole userRole
+     */
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
 }
