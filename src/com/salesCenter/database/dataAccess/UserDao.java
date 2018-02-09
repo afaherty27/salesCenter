c786151b92617152eded8d914dd0c206a2afbb8f
afaherty27 <afaherty27@gmail.com>
2018-02-09 00:22:38 -0600

    pseudo code for User and UserRole DAO classes

--- src/com/salesCenter/database/dataAccess/UserDao.java
+++ src/com/salesCenter/database/dataAccess/UserDao.java
@@ -1,5 +1,37 @@
 package com.salesCenter.database.dataAccess;
 
+import com.salesCenter.database.entity.User;
 
+/**
+ * Data Access Object for User table
+ * @author afaherty27
+ * @version 02/08/2018
+ */
 public class UserDao {
+
+    //instantiate private Session object
+
+    /**
+     * add user
+     */
+
+    /**
+     * Retrieve individual user from database
+     */
+
+    /**
+     * get list of all users
+     */
+
+    /**
+     * update User info in database
+     */
+
+    /**
+     * delete User from database
+     */
+
+    /**
+     * open new session for hibernate transaction
+     */
 }
--- src/com/salesCenter/database/dataAccess/UserRoleDao.java
+++ src/com/salesCenter/database/dataAccess/UserRoleDao.java
@@ -1,5 +1,35 @@
 package com.salesCenter.database.dataAccess;
 
+import com.salesCenter.database.entity.UserRole;
 
+/**
+ * Data Access Object for UserRole table
+ * @author afaherty27
+ * @version 1.0 on 02/08/2018
+ */
 public class UserRoleDao {
+
+    /**
+     * Add UserRole
+     */
+
+    /**
+     * Retrieve individual User from database
+     */
+
+    /**
+     * get list of all users
+     */
+
+    /**
+     * update UserRole info in database
+     */
+
+    /**
+     * deleve UserRole from database
+     */
+
+    /**
+     * open new session for hibernate transaction
+     */
 }
