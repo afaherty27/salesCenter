4f964295c66760cb2bef9e896ad5aea5ced5ef35
afaherty27 <afaherty27@gmail.com>
2018-02-09 00:27:08 -0600

    initial commit

---
+++ src/com/salesCenter/ApplicationStartup.java
@@ -0,0 +1,12 @@
+package com.salesCenter;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Start up class to initialize data for application
+ * @author afaherty27
+ * @version 1.0 on 02/08/2018
+ */
+public class ApplicationStartup extends HttpServlet {
+
+}
