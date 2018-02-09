b2f145353e23333c131d3b1880b1354da0ae4211
afaherty27 <afaherty27@gmail.com>
2018-02-08 23:31:02 -0600

    initial commit

--- src/com/salesCenter/database/dataAccess/SessionFactoryProvider.java
+++ src/com/salesCenter/database/dataAccess/SessionFactoryProvider.java
@@ -1,5 +1,33 @@
 package com.salesCenter.database.dataAccess;
 
-
+/**
+ * Create hibernate session instance
+ * @author afaherty27
+ * @version 1.0 on 02/08/2018
+ *
+ */
 public class SessionFactoryProvider {
+
+    /**
+     * Instantiate new SessionFactory object
+     * Load configuration details
+     */
+
+        // instantiate new Configuration obj
+
+        // apply mapping and properties in hibernate config file
+
+        // instantiate ServiceRegistry obj.  Apply value of Configuration object instance variable
+
+        //instantiate new SessionFactory.  apply properties and mappings
+
+    /**
+     * Instantiate SessionFactory obj if non exists
+     */
+
+        //if SessionFactory  is null
+
+            //create new SessionFactory obj
+
+        //return SessionFactory to program
 }
