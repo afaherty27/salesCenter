8459fa03e87362810109b5c484b4c3afa5b750ea
afaherty27 <afaherty27@gmail.com>
2018-02-09 00:34:52 -0600

    declared inst. variables.  Defined method requirements

--- src/com/salesCenter/ApplicationStartup.java
+++ src/com/salesCenter/ApplicationStartup.java
@@ -1,6 +1,11 @@
 package com.salesCenter;
 
+import com.salesCenter.database.dataAccess.UserDao;
+import com.salesCenter.database.dataAccess.UserRoleDao;
+import org.apache.log4j.Logger;
+
 import javax.servlet.http.HttpServlet;
+import java.util.Properties;
 
 /**
  * Start up class to initialize data for application
@@ -8,5 +13,30 @@ import javax.servlet.http.HttpServlet;
  * @version 1.0 on 02/08/2018
  */
 public class ApplicationStartup extends HttpServlet {
+
+    //create log4j Logger object instance
+    private final Logger log = Logger.getLogger(this.getClass());
+
+    // declare instance variables
+    private Properties  properties;
+    private UserDao     userDao;
+    private UserRoleDao userRoleDao;
+
+    /**
+     * Load properties and DAOs into application at startup
+     */
+
+    /**
+     * Load properties file into application
+     */
+
+    /**
+     * Instantiate new UserDao object
+     */
+
+    /**
+     * Instantiate new User object
+     */
+
 
 }
